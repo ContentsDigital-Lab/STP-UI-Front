0e4340cc77b2e12c9b03b6c1e369c480234d071b
Mister TP <thanakonp6@gmail.com>
2026-03-15 23:25:51 +0700

    env

--- lib/api/config.ts
+++ lib/api/config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "https://std.specterint.org/api";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://std.specterint.org/api";
 
 let isRedirectingToLogin = false;
 
